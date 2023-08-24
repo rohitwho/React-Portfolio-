be9e877c7d035eedb16d136a5efe1d39b6ee68a9
rohit nayyar <rohit@rohits-MacBook-Air.local>
2023-08-23 21:27:45 -0400

    deploy 3

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,8 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/React-Portfolio-",
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React-Portfolio-",
+
   plugins: [react()],
 })
